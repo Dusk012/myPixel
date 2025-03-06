86b41c400cf74d26ba19c355b74ea1a1cf729090
alegal07 <alegal07@ucm.es>
2025-03-06 10:25:42 +0100

    Añadido rutas
    Creadas rutas para contenido.
    TODO: implementar cada una de ellas.

--- ejercicio2_plantilla/src/contenido/router.js
+++ ejercicio2_plantilla/src/contenido/router.js
@@ -9,7 +9,68 @@ contenidoRouter.get('/normal', (req, res) => {
     }
     res.render('pagina', {
         contenido,
-        session: req.session
+        session: req.session,
+        error: null
+    });
+});
+
+contenidoRouter.get('/foro', (req, res) => {
+    let contenido = 'paginas/Usuarios/viewLogin';
+    if (req.session.login) {
+        contenido = 'paginas/foro/foro';
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        error: null
+    });
+});
+
+contenidoRouter.get('/desafios', (req, res) => {
+    let contenido = 'paginas/Usuarios/viewLogin';
+    if (req.session.login) {
+        contenido = 'paginas/desafios/desafios';
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        error: null
+    });
+});
+
+contenidoRouter.get('/tienda', (req, res) => {
+    let contenido = 'paginas/Usuarios/viewLogin';
+    if (req.session.login) {
+        contenido = 'paginas/tienda/shop';
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        error: null
+    });
+});
+
+contenidoRouter.get('/perfil', (req, res) => {
+    let contenido = 'paginas/Usuarios/viewLogin';
+    if (req.session.login) {
+        contenido = 'paginas/Usuarios/profile';
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        error: null
+    });
+});
+
+contenidoRouter.get('/panel', (req, res) => {
+    let contenido = 'paginas/Usuarios/viewLogin';
+    if (req.session.login) {
+        contenido = 'paginas/Usuarios/panel';
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        error: null
     });
 });
 
