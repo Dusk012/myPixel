d7b200a531bd41b8a6ba1f74017cbe8d0db93ce1
Pol Montero <153331258+polmonteroUCM@users.noreply.github.com>
2025-04-07 22:32:55 +0200

    Update router.js

--- ejercicio2_plantilla/src/usuarios/router.js
+++ ejercicio2_plantilla/src/usuarios/router.js
@@ -1,39 +1,34 @@
 import express from 'express';
-import { body } from 'express-validator';
-import { autenticado } from '../middleware/auth.js';
-import { viewLogin, perfilGet, actualizarFotoPerfil, darseDeBaja, doLogin, doLogout, viewSubmit, doSubmit, viewRegister, doRegister, sendComment } from './controllers.js';
-import asyncHandler from 'express-async-handler';
+import { body} from 'express-validator';
+
+import { viewLogin, perfilGet, actualizarFotoPerfil, darseDeBaja, doLogin, doLogout, viewSubmit, doSubmit, viewRegister, doRegister, sendComment/*, validateComment*/ } from './controllers.js';
+
 
 const usuariosRouter = express.Router();
 
+usuariosRouter.get('/login', viewLogin);
+usuariosRouter.post('/login', doLogin);
+usuariosRouter.get('/logout', doLogout);
 
+usuariosRouter.get('/submit', viewSubmit);
+usuariosRouter.post('/submit', doSubmit);
 
-usuariosRouter.get('/login', autenticado(null), asyncHandler(viewLogin));
-usuariosRouter.post('/login', autenticado(null, 'paginas/contenido/index'), 
-    body('username', 'No puede ser vacío').trim().notEmpty(), 
-    body('password', 'No puede ser vacío').trim().notEmpty(), 
-    asyncHandler(doLogin));
-usuariosRouter.get('/logout', asyncHandler(doLogout));
+usuariosRouter.post('/comentar'/*, validateComment*/, sendComment);
 
-usuariosRouter.get('/submit', asyncHandler(viewSubmit));
-usuariosRouter.post('/submit', asyncHandler(doSubmit));
-
-usuariosRouter.post('/comentar'/*, validateComment*/, asyncHandler(sendComment));
-
-usuariosRouter.get('/register', autenticado(null, '/paginas/contenido/index'), asyncHandler(viewRegister));
-usuariosRouter.post('/register', 
-    body('username', 'Sólo puede contener números y letras').trim().matches(/^[A-Z0-9]*$/i), 
-    body('username', 'No puede ser vacío').trim().notEmpty(), 
-    body('nombre', 'No puede ser vacío').trim().notEmpty(), 
-    body('password', 'La contraseña no tiene entre 6 y 10 caracteres').trim().isLength({ min: 6, max: 10 }), 
-    body('confirmPassword', 'La contraseña no coincide').custom((value, { req }) => {
+usuariosRouter.get('/register', viewRegister);
+usuariosRouter.post('/register'  
+    , body('username', 'Sólo puede contener números y letras').trim().matches(/^[A-Z0-9]*$/i)
+    , body('username', 'No puede ser vacío').trim().notEmpty()
+    , body('nombre', 'No puede ser vacío').trim().notEmpty()
+    , body('password', 'La contraseña no tiene entre 6 y 10 caracteres').trim().isLength({ min: 6, max: 10 })
+    , body('confirmPassword', 'La contraseña no coincide').custom((value, { req }) => {
     return value === req.body.password;
 })
-, asyncHandler(doRegister));
+, doRegister);
 
 // Ruta para actualizar la foto de perfil
-usuariosRouter.get('/perfil', asyncHandler(perfilGet));
-usuariosRouter.post('/perfil', asyncHandler(actualizarFotoPerfil));
-usuariosRouter.post('/darseDeBaja', asyncHandler(darseDeBaja));
+usuariosRouter.get('/perfil', perfilGet);
+usuariosRouter.post('/perfil', actualizarFotoPerfil);
+usuariosRouter.post('/darseDeBaja', darseDeBaja);
 
 export default usuariosRouter;
