2e0f6fac4cdccb5cf560a9e3b7f882969e7bfcb9
Dusk012 <zzhu@ucm.es>
2025-05-05 11:34:18 +0200

    Correcion de errores

--- ejercicio2_plantilla/src/utils/render.js
+++ ejercicio2_plantilla/src/utils/render.js
@@ -1,6 +1,17 @@
 import { error } from "./helpers.js";
 
 export function render(req, res, contenido, params) {
+    const publicPaths = [
+        'paginas/Usuarios/viewLogin',
+        'paginas/Usuarios/viewRegister',
+    ];
+    
+    // Check if the requested content requires authentication
+    const notRequiresAuth = !publicPaths.includes(contenido);
+    
+    if (notRequiresAuth && !req.session) {
+        contenido = 'paginas/Usuarios/noRegistrado';
+    }
     res.render('pagina', {
         contenido,
         session: req.session,
